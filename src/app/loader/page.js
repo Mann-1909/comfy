f5bdc92c5318484ba3d71f17e7a36cbdbe39786f
ajay jakhar <gargashwin9@gmail.com>
2023-09-18 20:34:39 +0530

    setup

--- src/app/loader/page.js
+++ src/app/loader/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Card from '../../components/common/Card'
+import Card from '../../../components/common/Card'
 
 export default function Page() {
   return (
     <div>
+        nsjvnsvnslvn
         <Card/>
     </div>
   )
