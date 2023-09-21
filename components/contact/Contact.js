4c8b6b3265e48820385bf9dc33ec87b071f82acd
mann-1909 <saxena.mann2005@gmail.com>
2023-09-21 15:08:48 +0530

    final1

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -1,31 +1,33 @@
-import React from 'react'
-import styles from './contact.module.css'
+import React from "react";
+import styles from "./contact.module.css";
 
 export default function Contact() {
   return (
     <div>
-        <div className={styles['wrapper']}>
-            <div className={styles['wrapper-left']}>
-                <form className={styles['form']}>
-                <div className={styles['contact']}>
-                    Contact
-                </div>
-                <input type='text' placeholder='Name' className={styles['name']}></input>
-                <input type='text' placeholder='Email' className={styles['email']}></input>
-                <textarea placeholder='Message' className={styles['message']}></textarea>
-                <div className={styles['button']}>
-                Send
-                </div>
-                </form>
-
-            </div>
-                
-
-
-
-
-            <div className={styles['wrapper-right']}></div>
+      <div className={styles["wrapper"]}>
+        <div className={styles["wrapper-left"]}>
+          <form className={styles["form"]}>
+            <div className={styles["contact"]}>Contact</div>
+            <input
+              type="text"
+              placeholder="Name"
+              className={styles["name"]}
+            ></input>
+            <input
+              type="text"
+              placeholder="Email"
+              className={styles["email"]}
+            ></input>
+            <textarea
+              placeholder="Message"
+              className={styles["message"]}
+            ></textarea>
+            <div className={styles["button"]}>Send</div>
+          </form>
         </div>
+
+        <div className={styles["wrapper-right"]}></div>
+      </div>
     </div>
-  )
+  );
 }
