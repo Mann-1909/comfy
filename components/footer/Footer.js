0788e4a37ac0f56ecc5dad2175f921996ffba2d7
Mann-1909 <saxena.mann2005@gmail.com>
2024-01-11 03:30:31 +0530

    v.0

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./footer.module.css";
 
-export default function () {
+export default function Footer(){
   return (
     <div>
       <div className={styles["wrapper"]}>
