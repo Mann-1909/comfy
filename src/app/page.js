1293b4f910060ccfedcba5d1a902eda0fa2e8692
Mann-1909 <saxena.mann2005@gmail.com>
2024-01-11 02:56:03 +0530

    v.0

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../../components/common/Navbar'
 import HeroPage from '../../components/hero-page/HeroPage'
+import Navbar from '../../components/common/Navbar'
 import About from '../../components/aboutus/About'
 import Contact from '../../components/contact/Contact'
 import Footer from '../../components/footer/Footer'
