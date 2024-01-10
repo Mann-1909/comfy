43a02949706225ea11c3f1b3ff30fd2be92510b9
Mann-1909 <saxena.mann2005@gmail.com>
2024-01-11 02:57:56 +0530

    v.0

--- components/hero-page/HeroPage.js
+++ components/hero-page/HeroPage.js
@@ -2,21 +2,20 @@ import React from "react";
 import styles from "./hero-page.module.css";
 
 export default function HeroPage() {
+
   return (
     <div>
       <div className={styles['wrapper']}>
         <div className={styles['text-wrapper']}>
           <div className={styles['text']}> 
-            Make your room comfy and beautiful.
+            Make your room Comfy and Beautiful.
           </div>
           <div className={styles['btn-wrapper']}>
             <div className={styles['shop']}>Shop</div>
             <div className={styles['contact']}>Contact</div>
           </div>
         </div>
-        <div className={styles['image']}>
-          
-        </div>
+        <div className={styles['image']}></div>
           
       </div>
     </div>
